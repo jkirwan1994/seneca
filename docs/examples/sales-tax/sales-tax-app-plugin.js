1a210abca374923b0aaf1afc6b5412ab086e2b83
Jack Kirwan <jkirwan1994@gmail.com>
2018-08-12 15:33:43 +0100

    Create plugin for sales-tax-app to handle different arguement structure

---
+++ docs/examples/sales-tax/sales-tax-app-plugin.js
@@ -0,0 +1,26 @@
+module.exports = function (options) {
+  var seneca = this
+  var plugin = 'shop'
+  var country = options.country || 'IE'
+  var rate = options.rate || 0.23
+ 
+  var calc = function (net) {
+    return net * (1 + rate)
+  }
+
+  seneca.add({ role: plugin, cmd: 'salestax', country: country }, function (msg, callback) {
+    var total = calc(parseFloat(msg.net, 10))
+    seneca.log.debug('apply-tax', msg.net, total, rate, country)
+    callback(null, { total: total })
+  })
+
+  seneca.add({ role: plugin, cmd: 'salestax' }, function (msg, callback) {
+    var total = calc(parseFloat(msg.args.query.net, 10))
+    seneca.log.debug('apply-tax', msg.args.query.net, total, rate, country)
+    callback(null, { total: total })
+  })
+  
+  return {
+    name: plugin
+  }
+}
