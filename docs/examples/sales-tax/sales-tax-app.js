df033c615ec104d01bcb8ff2afd4b49c6c101d3f
Jack Kirwan <jkirwan1994@gmail.com>
2018-08-12 21:07:19 +0100

    Changes to how sales-tax-app-plugin is called to reflect changes to seneca-web

--- docs/examples/sales-tax/sales-tax-app.js
+++ docs/examples/sales-tax/sales-tax-app.js
@@ -1,21 +1,30 @@
-var Connect = require('connect')
-var Connect_query = require('connect-query')
-var Body_parser = require('body-parser')
+var Express = require('express')
+var Web = require('seneca-web')
 
 var seneca = require('../../..')()
 
-seneca.use('sales-tax-plugin', {country: 'IE', rate: 0.23})
-seneca.use('sales-tax-plugin', {country: 'UK', rate: 0.20})
-seneca.use('sales-tax-plugin', {country: '*', rate: 0.25})
+var Routes = {
+  prefix: '/shop',
+  pin: 'role:shop, cmd:*',
+  map: {
+    salestax: { GET: true }
+  }
+}
+
+seneca.use('sales-tax-app-plugin', {country: 'IE', rate: 0.23})
+seneca.use('sales-tax-app-plugin', {country: 'UK', rate: 0.20})
+
+seneca.use(Web, {
+  context: Express(),
+  routes: Routes,
+  adapter: require('seneca-web-adapter-express'),
+  parseBody: false
+})
 
 seneca.ready(function () {
-  var app = Connect()
-  app.use(Connect_query())
-  app.use(Body_parser.json())
-  app.use(seneca.export('web'))
-
+  var app = seneca.export('web/context')()
+ 
   app.listen(3000)
 })
-// Uncomment these two lines to use the admin console
-// seneca.use('data-editor')
-// seneca.use('admin', {server: app, local: true})
+
+
